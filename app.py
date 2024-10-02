eda38df9b2d6f38d94651b5101fed26f14a971be
mcdm <marialucineaalves@gmail.com>
2024-10-02 15:40:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -111,29 +111,33 @@ st.pyplot(plt)
 st.subheader("2. Comparação das Alternativas")
 
 try:
-    matriz_alternativas = np.ones((num_alternativas, num_alternativas))
+    # Criação da matriz de resultados para todas as alternativas ponderadas pelos critérios
+    resultados_alternativas = np.zeros((num_alternativas, num_criterios))
 
     # Coleta comparações de preferências entre as alternativas para cada critério
-    for crit in criterios:
+    for crit_index, crit in enumerate(criterios):
         st.write(f"Comparação de alternativas para o critério: {crit}")
+        matriz_alternativas = np.ones((num_alternativas, num_alternativas))
+
         for i in range(num_alternativas):
             for j in range(i + 1, num_alternativas):
                 valor_alternativa = st.number_input(f"O quão preferível a alternativa {alternativas[i]} é em relação à alternativa {alternativas[j]} para o critério {crit} (escala 1-9)", min_value=1, max_value=9)
                 matriz_alternativas[i, j] = valor_alternativa
                 matriz_alternativas[j, i] = 1 / valor_alternativa
 
-    # Exibir a matriz de alternativas
-    df_matriz_alternativas = pd.DataFrame(matriz_alternativas, index=alternativas, columns=alternativas)
-    st.write(df_matriz_alternativas)
+        # Normalizar a matriz de alternativas
+        df_matriz_alternativas = pd.DataFrame(matriz_alternativas, index=alternativas, columns=alternativas)
+        normalizada_alternativas = NormalizingConsistency(df_matriz_alternativas)
+        st.write(normalizada_alternativas)
 
-    # Normalizar a matriz de alternativas
-    st.subheader("2.1 - Normalizando a Matriz de Alternativas")
-    normalizada_alternativas = NormalizingConsistency(df_matriz_alternativas)
-    st.write(normalizada_alternativas)
+        # Cálculo dos pesos das alternativas para este critério
+        _, pesos_alternativas = VV(normalizada_alternativas.to_numpy())
 
-    # Exibição dos resultados finais e pesos das alternativas
-    st.subheader("3. Resultado Final")
-    pesos_finais = np.dot(normalizada_alternativas, v)
+        # Armazenar os pesos das alternativas ponderados pelo peso do critério
+        resultados_alternativas[:, crit_index] = pesos_alternativas * v[crit_index]
+
+    # Cálculo dos pesos finais (somatório das alternativas ponderadas pelos critérios)
+    pesos_finais = np.sum(resultados_alternativas, axis=1)
     df_resultado = pd.DataFrame(pesos_finais, index=alternativas, columns=["Peso Final"])
     st.write(df_resultado)
 
