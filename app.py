e0ecf8134f4d8591439003c60d38b1887fd08db2
mcdm <marialucineaalves@gmail.com>
2024-10-04 22:46:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -66,9 +66,9 @@ st.markdown("<h3 style='text-align: center; background-color: #6495ED;'> Estrutu
 
 
 # Caixa de texto para o usuário digitar o título da pesquisa
-#titulo_pesquisa = st.text_input("Digite o título da pesquisa:", "Ranking das suas alternativas prioritárias")
-st.markdown("<h6 style='text-align: center;'>Inicie informando qual a decisão a ser tomada </h6>", unsafe_allow_html=True)
-titulo_pesquisa = st.text_input("", "Digite aqui seu problema", key="titulo")
+titulo_pesquisa = st.text_input("Digite o título da pesquisa:", "Inicie informando qual a decisão a ser tomada")
+#"st.markdown("<h6 style='text-align: center;'>Inicie informando qual a decisão a ser tomada </h6>", unsafe_allow_html=True)
+"titulo_pesquisa = st.text_input("", "Digite aqui", key="titulo")
 
 num_alternativas = st.number_input("Quantas alternativas serão analisadas?", min_value=2, step=1)
 alternativas = []
