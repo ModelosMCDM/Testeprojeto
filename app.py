941b8f00c34bd57c427095ab29d14751f3b67290
mcdm <marialucineaalves@gmail.com>
2024-10-05 13:07:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -241,3 +241,43 @@ else:
 
     except Exception as e:
         st.error(f"Ocorreu um erro ao processar as comparações de alternativas: {e}")
+
+
+st.write( "TESTET ETETETETTETETE exemplo" ) # insere texto informativo 
+# Exemplo de DataFrame com dados fictícios
+df_resultado = pd.DataFrame({
+    'Alternativa': ['A', 'B', 'C', 'D', 'E'],
+    'Peso Final': [0.2, 0.4, 0.1, 0.3, 0.5]
+})
+df_resultado = df_resultado.set_index('Alternativa')
+
+titulo_pesquisa = "Melhoria de Processo"  # Apenas um exemplo
+
+# Função para atualizar o gráfico em cada frame
+def update(num):
+    plt.clf()  # Limpa o gráfico para cada nova frame
+    plt.figure(figsize=(27, 8))  # Largura e altura do gráfico
+    plt.title(f"Ranking para problema de: {titulo_pesquisa}", fontsize=36, pad=45)
+
+    # Atualiza os dados com uma leve mudança para criar o efeito de animação
+    df_resultado['Peso Final'] = df_resultado['Peso Final'].sample(frac=1).reset_index(drop=True)
+
+    # Desenha o gráfico com as barras atualizadas
+    ax = sns.barplot(x=df_resultado.index, y=df_resultado["Peso Final"], palette="viridis")
+
+    # Aumenta o tamanho das legendas dos eixos
+    ax.set_ylabel("Peso Final", fontsize=30)  # Tamanho da fonte do eixo Y
+    ax.tick_params(axis='x', labelsize=30)  # Tamanho da fonte para os rótulos do eixo X
+    ax.tick_params(axis='y', labelsize=30)  # Tamanho da fonte para os rótulos do eixo Y
+
+    # Adiciona os valores em cima de cada barra
+    for p in ax.patches:
+        height = p.get_height()
+        ax.text(p.get_x() + p.get_width() / 2, height + 0.01, '{:1.2f}'.format(height), ha='center', fontsize=34)
+
+# Criando a animação
+fig = plt.figure(figsize=(27, 8))
+anim = FuncAnimation(fig, update, frames=10, repeat=False)
+
+# Exibir a animação no Streamlit
+st.pyplot(fig)
