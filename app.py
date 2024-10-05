a3f6259aa9ea8bc506898c05eb342440b5b74768
mcdm <marialucineaalves@gmail.com>
2024-10-04 21:18:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -157,10 +157,10 @@ else:
 
         # Aumentando o tamanho das legendas dos eixos
         #ax.set_xlabel("Alternativas", fontsize=18)  # Tamanho da fonte do eixo X
-        ax.set_ylabel("Peso Final", fontsize=18)  # Tamanho da fonte do eixo Y
+        ax.set_ylabel("Peso Final", fontsize=18, pad=20)  # Tamanho da fonte do eixo Y
         # Aumentar o tamanho da fonte das legendas dos eixos
-        ax.tick_params(axis='x', labelsize=18)  # Tamanho da fonte para os rótulos do eixo X
-        ax.tick_params(axis='y', labelsize=18)  # Tamanho da fonte para os rótulos do eixo Y
+        ax.tick_params(axis='x', labelsize=22)  # Tamanho da fonte para os rótulos do eixo X
+        ax.tick_params(axis='y', labelsize=22)  # Tamanho da fonte para os rótulos do eixo Y
 
         for p in ax.patches:
             height = p.get_height()
