c1162419a9ad99085dfe8974f089c35ba483eb2f
mcdm <marialucineaalves@gmail.com>
2024-10-05 12:42:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -102,9 +102,8 @@ titulo_pesquisa = st.text_input("Digite aqui o título da pesquisa")
 
 
 #num_alternativas = st.number_input("Quantas alternativas serão analisadas?", min_value=2, step=1)
-# Adicionando o título em negrito
 st.markdown("<strong>Quantas alternativas serão analisadas?</strong>", unsafe_allow_html=True)
-num_alternativas = st.number_input("", min_value=2, step=1)
+num_alternativas = st.number_input("", min_value=2)
 st.write(f"Você selecionou {num_alternativas} alternativas.")
 
 alternativas = []
