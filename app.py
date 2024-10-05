d1692d842cf16cb70de719c5ae4de6ec799744a2
mcdm <marialucineaalves@gmail.com>
2024-10-04 21:05:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -149,29 +149,21 @@ else:
         df_resultado = pd.DataFrame(pesos_finais, index=alternativas, columns=["Peso Final"])
         st.write(df_resultado)
 
-        # Resultado final
+       # Resultado final
         st.subheader("4. Resultado final")
-        plt.figure(figsize=(27, 8))  # largura e altura
-        
-        # Título do gráfico
-        plt.title("Ranking das suas alternativas prioritárias", fontsize=20)  # Aumentar o tamanho da fonte do título
-        
+        plt.figure(figsize=(27,8))  # largura e altura
+        plt.title("Ranking das suas alternativas prioritárias", fontsize=20)  # Aumente o tamanho da fonte aqui
         ax = sns.barplot(x=df_resultado.index, y=df_resultado["Peso Final"], data=df_resultado)
         
-        # Aumentar o tamanho da fonte dos rótulos dos eixos
-        ax.set_xlabel("Alternativas", fontsize=18)  # Eixo X
-        ax.set_ylabel("Peso Final", fontsize=18)    # Eixo Y
+        # Aumentando o tamanho das legendas dos eixos
+        ax.set_xlabel("Alternativas", fontsize=18)  # Tamanho da fonte do eixo X
+        ax.set_ylabel("Peso Final", fontsize=18)  # Tamanho da fonte do eixo Y
         
-        # Aumentar o tamanho da fonte das legendas dos eixos
-        ax.tick_params(axis='x', labelsize=14)  # Tamanho da fonte para os rótulos do eixo X
-        ax.tick_params(axis='y', labelsize=14)  # Tamanho da fonte para os rótulos do eixo Y
-        
-        # Adicionando texto nas barras
         for p in ax.patches:
             height = p.get_height()
-            ax.text(p.get_x() + p.get_width() / 2, height + 0.01, '{:1.2f}'.format(height), ha='center', fontsize=16)  # Aumentar o tamanho se necessário
+            ax.text(p.get_x() + p.get_width() / 2, height + 0.01, '{:1.2f}'.format(height), ha='center', fontsize=16)  # Aumente aqui se necessário
         
-        plt.show()  # Exibir o gráfico
+        plt.show()  # Não esqueça de exibir o gráfico
 
     except Exception as e:
         st.error(f"Ocorreu um erro ao processar as comparações de alternativas: {e}")
