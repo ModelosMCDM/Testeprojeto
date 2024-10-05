adb160f3f32aa1b005468e019b1b7f8bf2c1f348
mcdm <marialucineaalves@gmail.com>
2024-10-05 00:16:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ html_temp = """
     <h3 style="color: black; margin-bottom: 10px;">Metodologia de apoio à decisão </h3>
     <p style="color: black; margin-bottom: 10px;"">Modo de uso: Digite a quantidade de alternativas e critérios que você usará para a tomada de decisão. Em seguida, preencha as opções conforme necessário </p>
     <p style="color: black; margin-bottom: 10px;"">Com base na métodologia de Thomas Saaty - Por Jaqueline Alves </p>
+    <p style="color: black; margin-bottom: 10px;"">Código fonte, devidamente patenteado no INPI </p>
     <p style="color: black; margin-bottom: 10px;"">Todos os cálculos dos métodos irão funcionar automaticamente</p>
 </div>
 
